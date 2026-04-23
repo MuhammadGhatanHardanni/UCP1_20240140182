81c5aac441b4d26670bfe42a1d6eb7dd994f1e6d
Ghatan55 <ghatanhardanni@gmail.com>
2026-04-23 09:57:57 +0700

    eror

--- script.js
+++ script.js
@@ -4,51 +4,7 @@ let dataAnggota = [
   { nama: "Aryya", email: "Aryya@mail.com", minat: "King MRC" },
 ];
 
-let tabel = document.getElementById("tabelAnggota");
 
-if (tabel !== null) {
-  for (let i = 0; i < dataAnggota.length; i++) {
-    tabel.innerHTML +=
-      "<tr>" +
-      "<td>" +
-      dataAnggota[i].nama +
-      "</td>" +
-      "<td>" +
-      dataAnggota[i].email +
-      "</td>" +
-      "<td>" +
-      dataAnggota[i].minat +
-      "</td>" +
-      "</tr>";
-  }
-}
-
-let formPendaftaran = document.getElementById("formDaftar");
-
-if (formPendaftaran !== null) {
-  formPendaftaran.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    let namaDiinput = document.getElementById("nama").value;
-    let emailDiinput = document.getElementById("email").value;
-    let minatDiinput = document.getElementById("minat").value;
-
-    dataAnggota.push({
-      nama: namaDiinput,
-      email: emailDiinput,
-      minat: minatDiinput,
-    });
-
-    alert(
-      "Pendaftaran Sukses!\nNama: " + namaDiinput + "\nMinat: " + minatDiinput,
-    );
-
-    document.getElementById("hasilInput").innerHTML =
-      "Data " + namaDiinput + " berhasil disimpan!";
-
-    formPendaftaran.reset();
-  });
-}
 
 
 function gantiGambar() {
